a1255db88e36464be7a1369652a3515bfc4f7e61
amal-dev-01 <devmpamal@gmail.com>
2023-06-19 17:52:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import './component/Btn.css'
+
+const color2 = {
+  backgroundColor: "#282c34",
+  minHeight: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white"
+};
+const color1={
+  backgroundColor:"white",
+  minHeight: "100vh",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "black"
+}
 
 function App() {
+  const [color,setColor]=useState(color1)
+  const Change=()=>{
+    if(color==color1){
+    setColor(color2)
+  }
+  else{
+    setColor(color1)
+  }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div style={color}>
+
+   <button onClick={Change} className='btn'>Change Theme</button>
+   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima,
+    inventore tempora iusto impedit distinctio odit eos cumque. Minus blanditiis,
+     perspiciatis doloremque porro explicabo quam assumenda quos sit distinctio aliquam fuga.</p>
+
+   </div>
   );
 }
 
